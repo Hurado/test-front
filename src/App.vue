76a9087577b73131549319eff4425adac94bb199
Hurado <piaszczynskiarkadiusz@gmail.com>
2022-07-22 13:52:45 +0200

    Adding first logic skeleton for quizz app

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,8 @@
 <script lang="ts">
 import { ref, computed } from "vue";
 
-const questions = ref({
+const questions = ref(
+  {
   question: "What is VueJS?",
   answer: 0,
   options: [
@@ -14,7 +15,53 @@ const questions = ref({
     "Extension for JS",
     "Another Language",
   ],
+  selected: null
+},
+  {
+  question: "What is Vuex?",
+  answer: 2,
+  options: [
+    "Framework for Vue",
+    "A library JS",
+    "State management library",
+    "Approach of writing code in Vue",
+  ],
+  selected: null
+},
+  {
+  question: "What is Vite?",
+  answer: 3,
+  options: [
+    "Newest package of node for Vue",
+    "State management library",
+    "A Library Vue",
+    "An alternative for Webpack",
+  ],
+  selected: null
 });
+
+const quizCompleted = ref(false)
+
+const currentQuestion = ref(0)
+const score = computed (()=> {
+  let value = 0;
+  questions.value.map(q => {
+    if (q.selected === q.answer) {
+      value++
+    }
+  })
+  return value
+})
+
+const getCurrentQuestion = computed(() => {
+  let question = questions.value[currentQuestion.value]
+  question.index = currentQuestion.value
+  return question
+})
+
+const setAnswer = evt => {
+  questions.value[currentQuestion.value].selected = evt.target.value
+}
 </script>
 
 <style>
